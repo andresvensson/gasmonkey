dcc64e6a7afbf075e016620984d229c2b60bdd75
slicemenice <andresvensson2@gmail.com>
2022-08-02 19:50:27 +0200

    added stats page

---
+++ stats/numbers.php
@@ -0,0 +1,49 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include '../includes/dbh.gas.php';
+
+
+# Create this central php file
+# to provide stats and variables
+# to be used in main index (stats column) and
+# dedicated statistics page (with graphs) at
+# gas.andresvensson.se/stats
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src='includes/Chart.js'></script>
+    
+    <title>Stats</title>
+</head>
+<body style="background-color:#e2fcd4;">
+<center>
+
+
+
+Soon to be...
+
+
+
+
+
+<br><br>
+
+<br>
+
+
+
+</center>
+
+
+</body>
+</html>
